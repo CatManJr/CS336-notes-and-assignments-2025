9f56e5acb865544f64ad6faa49b14b81ed6143af
CatManJr <2396242639@qq.com>
2025-05-30 22:27:21 +0800

    Update: Profile & Flash Attetion2 in pytorch and triton. Only test the pytorch version.

--- assignments/assignment2-systems/tests/adapters.py
+++ assignments/assignment2-systems/tests/adapters.py
@@ -4,35 +4,39 @@ from typing import Type
 
 import torch
 
+# Import our FlashAttention implementations
+from cs336_systems.flash_attention import (
+    FlashAttentionPyTorchFunction, 
+    FlashAttentionTritonFunction,
+    TRITON_AVAILABLE
+)
 
 
 def get_flashattention_autograd_function_pytorch() -> Type:
     """
-    Returns a torch.autograd.Function subclass that implements RMSNorm.
-    The expectation is that this class will implement RMSNorm
+    Returns a torch.autograd.Function subclass that implements FlashAttention-2.
+    The expectation is that this class will implement FlashAttention-2
     using standard PyTorch operations.
 
     Returns:
         A class object (not an instance of the class)
     """
-    # For example: return MyRMSNormAutogradFunctionClass
-    raise NotImplementedError
+    return FlashAttentionPyTorchFunction
 
 
 def get_flashattention_autograd_function_triton() -> Type:
     """
-    Returns a torch.autograd.Function subclass that implements RMSNorm
+    Returns a torch.autograd.Function subclass that implements FlashAttention-2
     using Triton kernels.
     The expectation is that this class will implement the same operations
-    as the class you return in get_rmsnorm_autograd_function_pytorch(),
+    as the class you return in get_flashattention_autograd_function_pytorch(),
     but it should do so by invoking custom Triton kernels in the forward
     and backward passes.
 
     Returns:
         A class object (not an instance of the class)
     """
-    # For example: return MyTritonRMSNormAutogradFunctionClass
-    raise NotImplementedError
+    return FlashAttentionTritonFunction
 
 
 def get_ddp_individual_parameters(module: torch.nn.Module) -> torch.nn.Module:
